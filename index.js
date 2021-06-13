7fefdf1f7b6daaae930448755280bc6466d0474b
Benni-beep <krikelkr4kel@gmail.com>
2021-06-13 20:27:30 +0200

    added abilityMods /w sign, stylesheet

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const root = document.getElementById("root");
 // });
 // const sheetHeaderEntries = sheetLists.sheetHeaderEntries;
 
+/**
+ * proficiency bonus value input -> text field 
+ */
+
 const sheetHeaderEntries = [
     "character name",
     "class",
@@ -17,7 +21,8 @@ const sheetHeaderEntries = [
     "level",
     "background",
     "alignment",
-    "race"
+    "race",
+    "proficiency bonus"
 ];
 const abilitiesAndSkills = {
     "strength": [
@@ -80,34 +85,76 @@ const sheetHeader = (sheetHeaderEntries) => {
             headerEntryInput.value = 1;
             headerEntryInput.min = 1;
             headerEntryInput.max = 20;
+
+            headerEntryInput.addEventListener("input", () => {
+
+                const characterLevel = document.getElementById("sheet-header-level");
+                let proficiencyBonus = document.getElementById("sheet-header-proficiency-bonus");
+                const proficiencyIncreaseThresholds = [5, 9, 13, 17];
+                let proficiencyBonusIncrease = 0;
+
+                proficiencyIncreaseThresholds.forEach(thresholdLevel => {
+                    if (characterLevel.value >= thresholdLevel) { proficiencyBonusIncrease++ }
+                });
+
+                proficiencyBonus.value = 2 + proficiencyBonusIncrease;
+
+            });
+        };
+
+        if (labelText === "proficiency bonus") {
+            headerEntryInput.type = "number";
+            headerEntryInput.value = 2;
+            headerEntryInput.min = 2;
+            headerEntryInput.max = 6;
+            headerEntryInput.disabled = true;
         };
 
         header.appendChild(headerEntry);
-    })
+    });
     root.appendChild(header);
 };
 
+const modifierSign = n => n < 0 ? "" : "+";
+
 const attributeList = (abilitiesAndSkills) => {
+
     const abilities = Object.keys(abilitiesAndSkills);
-    const abilityList = document.createElement("div");
+    const abilityList = document.createElement("ul");
+
     abilities.forEach(ability => {
 
-        const abilityListElement = document.createElement("div");
-        const nameText = document.createTextNode(ability);
+        const listElement = document.createElement("li");
+        const text = document.createTextNode(ability);
         const label = document.createElement("label");
         const abilityScore = document.createElement("input");
+        const abilityModifierSign = document.createElement("span");
+        const abilityModifier = document.createElement("span");
 
-        label.appendChild(nameText);
-        abilityListElement.appendChild(label);
-        abilityListElement.id = `ability-${ability}`
-        abilityListElement.appendChild(abilityScore);
+        label.appendChild(text);
+        listElement.appendChild(label);
+        listElement.id = `ability-${ability}`
+        listElement.appendChild(abilityScore);
         abilityScore.type = "number";
         abilityScore.value = 10;
         abilityScore.min = 3;
         abilityScore.max = 18;
-        abilityList.appendChild(abilityListElement)
+        listElement.appendChild(abilityModifierSign);
+        listElement.appendChild(abilityModifier);
+        abilityModifier.textContent = 0;
+        abilityModifierSign.textContent = modifierSign(abilityModifier.textContent);
+        abilityScore.addEventListener("input", e => {
+            const score = e.target.value;
+            const mod = Math.floor((score - 10) / 2);
+            abilityModifier.textContent = mod;
+            abilityModifierSign.textContent = modifierSign(abilityModifier.textContent);
+        });
+        listElement.appendChild(abilityModifier);
+
+        abilityList.appendChild(listElement)
         abilityList.id = "ability-list"
     });
+
     root.appendChild(abilityList);
 };
 
@@ -116,7 +163,7 @@ function getAbilityNameBySkillName(object, value) {
 };
 
 const skillsAndDependenciesList = (abilitiesAndSkills) => {
-    const skillListWithDependencies = document.createElement("div");
+    const skillListWithDependencies = document.createElement("ul");
     const abilities = Object.keys(abilitiesAndSkills);
     let skillNames = [];
     abilities.forEach(ability => {
@@ -125,7 +172,7 @@ const skillsAndDependenciesList = (abilitiesAndSkills) => {
     skillNames.sort();
     skillNames.forEach(skill => {
 
-        const skillListElement = document.createElement("div");
+        const skillListElement = document.createElement("li");
         const skillDependency = getAbilityNameBySkillName(abilitiesAndSkills, skill)
         const abilityNameText = document.createTextNode(`${skill} (${skillDependency})`);
         const abilityLabel = document.createElement("label");
@@ -135,11 +182,22 @@ const skillsAndDependenciesList = (abilitiesAndSkills) => {
         skillListElement.id = `skill-${skill.replaceAll(" ", "-")}`;
         skillListWithDependencies.appendChild(skillListElement);
         skillListWithDependencies.id = "skill-list";
-    })
+
+        /**
+         * for each skill:
+         * name
+         * attribute it depends on
+         * proficiency checkbox
+         * value that depends on PB and proficiencies
+         * add event listener to each value-element if its corresponding checkbox is checked
+         * 
+         * replace number fields with something else / div.textContent?
+         */
+
+    });
 
     root.appendChild(skillListWithDependencies);
 };
-
 
 sheetHeader(sheetHeaderEntries);
 attributeList(abilitiesAndSkills);
